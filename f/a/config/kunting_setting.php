4c84de1bf33ae57ead37648b5d82a333412cd4f5
kunting <kunting@219.234.2.78>
2013-06-26 00:27:38 +0800

    change

--- f/a/config/kunting_setting.php
+++ f/a/config/kunting_setting.php
@@ -12,13 +12,10 @@ $config['user_group'] = array(
 $congfig['groupbuy_s'] = array(
 	'1' => "开启",
 	'0' => "关闭"
-<<<<<<< HEAD
-=======
 );
 $config['category'] = array(
 	'article' => array('id'=>'1','name' => "资讯"),
 	'actions' => array('id'=>'2','name' => "活动"),
 	'groupbuy' => array('id'=>'3','name' => "团购"),
 	'sharepage' => array('id'=>'4','name' => "分享"),
->>>>>>> 7a36c9cd96a2750e3c87a8b5212c844be58f4362
 );
