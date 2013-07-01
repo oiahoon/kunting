6f59573ff8bab33959e4cd328629d6413c330583
Huang Yuyao <4296411@qq.com>
2013-07-01 23:49:26 +0800

    changge

--- f/a/views/sharepage.php
+++ f/a/views/sharepage.php
@@ -5,7 +5,7 @@
 <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
   <head>
     <meta charset="utf-8">
-    <title><?php echo $title['top']. " - " .$title['small'];?></title>
+    <title><?php echo $title['small'];?></title>
     <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
